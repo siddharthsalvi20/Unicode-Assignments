b6b394fd157b18e900928bd1377857c0aab845bc
siddharthsalvi20 <54187467+siddharthsalvi20@users.noreply.github.com>
2019-10-03 10:34:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import Counter from './Counter.js';
+import Click from './Click.js';
+class NameForm extends React.Component {
+ 
+
+  render() {
+    return (
+        <div>
+     
+        <Click />
+        
+        </div>
+    );
+  }
+}
+export default NameForm
---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+class Sid extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            count:0
+        }
+    }
+    increment(){
+        this.setState({
+            count:this.state.count+1
+        })
+    }
+render(){
+    return(
+    <div>
+        <div>Count:{this.state.count}</div>
+        <button onClick={()=>this.increment()}>Increment</button>
+</div> 
+    );
+    
+}
+}
+export default Sid;
