457fe2637be6a0806240c10325f925f66dd0b974
siddharthsalvi20 <54187467+siddharthsalvi20@users.noreply.github.com>
2019-10-24 00:55:25 +0530

    Add files via upload

---
+++ ZomatoApi_Task/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+import Click from './Components/zomatoapi.js';
+class NameForm extends React.Component {
+ 
+
+  render() {
+    return (
+        <div>
+     
+        <Click />
+        
+        </div>
+    );
+  }
+}
+export default NameForm
